9ac02b8ff6bc502c3c8b32e324b6a427bfed3aae
Han Yang <786213774@qq.com>
2017-05-02 23:38:13 +0800

    17-5-2修改

--- Js-6/JS/app.js
+++ Js-6/JS/app.js
@@ -53,4 +53,71 @@ angular.module('transmit',['ui.router'])
         return sta;
     }
 });
+var studentModule=angular.module("studentModule",[]);
+studentModule.controller("studentController",function($scope,$http){
+$scope.currentPage=1;
+$scope.maxPage=0;
+$scope.isLastAble="no-drop";
+$scope.isNextAble="pointer";
+$scope.searchType="s_name";
+$scope.createTable=function(data,curpage){
+$scope.students=data;
+// console.log(data);
+var arr=[];
+for(var i=1;i<=data.maxpage;i++){
+arr.push(i);
+}
+$scope.page=arr;
+$scope.currentPage=curpage;
+$scope.maxPage=data.maxpage;
+if($scope.currentPage!=1){
+$scope.isLastAble="pointer";
+}else{
+$scope.isLastAble="no-drop";
+}
+if($scope.currentPage!=$scope.maxPage){
+$scope.isNextAble="pointer";
+}else{
+$scope.isNextAble="no-drop";
+}
+};
+$scope.searchStu=function(curpage,eachpage){
+$scope.maxPage=0;
+$http.post("/students/searchStudent",{type:$scope.searchType,val:$scope.searchVal,curpage:curpage,eachpage:eachpage})
+.success(function(data){
+$scope.createTable(data,curpage);
+})
+};
+$scope.searchStu(1,5);
+$scope.toLastPage=function(){
+if($scope.currentPage!=1){
+$scope.searchStu($scope.currentPage-1,5);
+}else{
+return 0;
+}
+};
+$scope.toNextPage=function(){
+if($scope.currentPage!=$scope.maxPage){
+$scope.searchStu($scope.currentPage+1,5);
+}else{
+return 0;
+}
+};
+$scope.deleteStu=function(id){
+$http.get("/students/deleteStudent?id="+id+"").success(function(data){
+$scope.searchStu(1,5);
+})
+};
+$scope.addStu=function(){
+$http.post("/students/addStudent",{name:$scope.addName,age:$scope.addAge,sex:$scope.addSex})
+.success(function(data){
+$scope.searchStu($scope.maxPage,5);
+})
+};
+$scope.changeStu=function(){
+$http.post("/students/changeStudent",{name:$scope.configName,age:$scope.configAge,sex:$scope.configSex,id:$scope.configId}).success(function(data){
+$scope.searchStu($scope.currentPage,5);
+})
+}
+});
 
